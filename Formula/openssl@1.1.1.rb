48e04ca0ba323f45d476b8fd9874e859141d7699
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-06-18 02:07:21 +0900

    見つからないmirrorを削除

--- Formula/openssl@1.1.1.rb
+++ Formula/openssl@1.1.1.rb
@@ -5,8 +5,8 @@ class OpensslAT111 < Formula
   mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.1.1w.tar.gz"
   mirror "http://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.1.1w.tar.gz"
   mirror "https://www.openssl.org/source/old/1.1.1/openssl-1.1.1w.tar.gz"
-  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/old/1.1.1/openssl-1.1.1w.tar.gz"
-  mirror "http://www.mirrorservice.org/sites/ftp.openssl.org/source/old/1.1.1/openssl-1.1.1w.tar.gz"
+  # mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/old/1.1.1/openssl-1.1.1w.tar.gz"
+  # mirror "http://www.mirrorservice.org/sites/ftp.openssl.org/source/old/1.1.1/openssl-1.1.1w.tar.gz"
   sha256 "cf3098950cb4d853ad95c0841f1f9c6d3dc102dccfcacd521d93925208b76ac8"
   license "OpenSSL"
   version_scheme 1
