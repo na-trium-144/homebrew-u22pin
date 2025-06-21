200926dd5259d1ab916a1aebb77233f531b386fd
k. Naka <100704180+na-trium-144@users.noreply.github.com>
2025-06-21 17:28:32 +0000

    google-benchmark@1.6: add 1.6.2 bottle.

--- Formula/google-benchmark@1.6.rb
+++ Formula/google-benchmark@1.6.rb
@@ -6,6 +6,13 @@ class GoogleBenchmarkAT16 < Formula
   license "Apache-2.0"
   head "https://github.com/google/benchmark.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/na-trium-144/homebrew-u22pin/releases/download/google-benchmark@1.6-1.6.2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ceec2c0b54f013a61707f8025b5d97d0097b1ddd2eee0c8bd95beaa00ddee759"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "08c35e776129b1f3f446ffbcd08e9e5f01c6aeb1c9d83e3a6c739ee5a133aca6"
+    sha256 cellar: :any_skip_relocation, ventura:       "7887900c4f9cc8aabccf2dffc94982d7f0ead7eba0bf21681a0002645b54a025"
+  end
+
   keg_only :versioned_formula
 
   depends_on "cmake@3.22" => :build
